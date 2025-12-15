48e76317fcc4c04ae6179ad6c3826557d3827a4d
RnK-web-pc <yann.daugrois@gmail.com>
2025-12-15 01:01:29 +0100

    Notification service integration with MailTrap to send mails when order is created.

--- notification-service/src/main/java/com/rdgs/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/rdgs/notificationservice/NotificationServiceApplication.java
@@ -4,9 +4,17 @@ import com.rdgs.notificationservice.event.OrderCreatedEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
+
+    private final JavaMailSender mailSender;
+
+    public NotificationServiceApplication(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
@@ -14,7 +22,16 @@ public class NotificationServiceApplication {
 
     @KafkaListener(topics = {"product-orders"}, groupId = "notifications-consumer-group")
     public void listen(OrderCreatedEvent event) {
-        System.out.println(event);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("notification-service@mail.com");
+        message.setTo(event.email());
+        message.setSubject("Your order has been created");
+        message.setText("""
+                Hello,
+                
+                Your order has been created!
+                Order id : """ + event.orderId());
+        mailSender.send(message);
     }
 
 }

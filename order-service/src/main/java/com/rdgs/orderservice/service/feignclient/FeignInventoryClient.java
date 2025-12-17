0da882719a7d8442bef13387638112b7c8ff8a40
RnK-web-pc <yann.daugrois@gmail.com>
2025-12-17 22:44:45 +0100

    Fixed Feign client loadbalancer error + removed services from docker compose

--- order-service/src/main/java/com/rdgs/orderservice/service/feignclient/FeignInventoryClient.java
+++ order-service/src/main/java/com/rdgs/orderservice/service/feignclient/FeignInventoryClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("${inventory.service.uri}")
+@FeignClient(value = "inventory-service", url="${inventory.service.uri}")
 public interface FeignInventoryClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/api/v1/inventory")
